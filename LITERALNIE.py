2bff5c260b73257351600f7511a03ff9200d5857
Daniel <122170174+Kaphire@users.noreply.github.com>
2023-04-25 15:58:36 +0200

    Add files via upload
    v2
    added init functions and classes
    started working on user interface

--- LITERALNIE.py
+++ LITERALNIE.py
@@ -7,59 +7,84 @@ print('Game rules: blablabla')
 
 ##      ***DICTIONARY OF 5 LETTER WORDS***
 
-##      ***GAME LOGIC***
-
 # Wygenerowanie losowej liczby porządkowej słowa z pliku excel
+class backend:
+    def __init__(self):    
+        import random
+        l_porzadkowa = random.randint(1,5)
 
-import random
-l_porzadkowa = random.randint(1,5)
+        # Podczyt słowa 5 literowego z excela
 
-# Podczyt słowa 5 literowego z excela
+        from openpyxl import load_workbook
+        wb = load_workbook('SLOWA 5 LITEROWE.xlsx')
+        sheet = wb.active
+        szukane_słowo = sheet.cell(row = l_porzadkowa, column = 2).value
 
-from openpyxl import load_workbook
-wb = load_workbook('SLOWA 5 LITEROWE.xlsx')
-sheet = wb.active
-szukane_słowo = sheet.cell(row = l_porzadkowa, column = 2).value
+        print(szukane_słowo, len(szukane_słowo), 'Generating sucessful')
 
-print(szukane_słowo, len(szukane_słowo), 'Generating sucessful')
+        #stworzenie listy z szukanym słowem
+        global lista_szukane
+        lista_szukane = []
+        for x in szukane_słowo:
+            lista_szukane.append(x)
+        print(lista_szukane)
 
-#stworzenie listy z szukanym słowem
-lista_szukane=[]
-for x in szukane_słowo:
-    lista_szukane.append(x)
-print(lista_szukane)
+    ##      ***GAME LOGIC***
 
-# zabezpieczenie przed wpisaniem złego słowa
-while True:
-    guess_1 = input('Type your guess: ')
-    if len(guess_1) == 5:
-        print("Word lenght correct")
-        break    
-    print("Wrong word lenght, input 5 letter word.")
+    def Logika_gry():
+        # zabezpieczenie przed wpisaniem złego słowa
+        while True: 
+            global guess_1
+            guess_1 = input('Type your guess: ')
+            if len(guess_1) == 5:
+                print("Word lenght correct")
+                break    
+            print("Wrong word lenght, input 5 letter word.")
 
-#stworzenie listy ze zgadywanym słowem 1
-lista_guess_1=[]
-for x in guess_1:
-    x = x.upper()
-    lista_guess_1.append(x)
-print(lista_guess_1)
+        #stworzenie listy ze zgadywanym słowem 1
+        global lista_guess_1
+        lista_guess_1=[]
+        for x in guess_1:
+            x = x.upper()
+            lista_guess_1.append(x)
+        print(lista_guess_1)
 
-#porównywanie liter w obydwu słowach
-szukane_słowo_podpowiedź = ['_','_','_','_','_']
+        #porównywanie liter w obydwu słowach
+        szukane_słowo_podpowiedź = ['_','_','_','_','_']
 
-i=0
-while i<=4:
-    for x in lista_guess_1:
-        if x == lista_szukane[i]:
-            szukane_słowo_podpowiedź[i] = x
-    i=i+1
+        i=0
+        while i<=4:
+            for x in lista_guess_1:
+                if x == lista_szukane[i]:
+                    szukane_słowo_podpowiedź[i] = x
+            i=i+1
 
-print('Poszukiwane słowo jest postaci: ', szukane_słowo_podpowiedź)
-
-
-#if lista_guess_1[i] == lista_szukane[i]:
-#       print(f'{lista_guess_1(i)} znajduje się na prawidłowej pozycji {i}')
-#       i=i+1
-
+        print('Poszukiwane słowo jest postaci: ', szukane_słowo_podpowiedź)
 
 ##      *** VISUAL INTERFACE ***
+class frontend(backend):
+    def __init__(self):
+        from PyQt6 import QtWidgets
+        from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QLineEdit
+        import sys
+
+        app = QApplication(sys.argv)
+        win = QMainWindow()
+        win.setGeometry(200, 200, 500, 500)
+        win.setWindowTitle('LITERALNIE')
+
+        label = QtWidgets.QLabel(win)
+        label.setText('Guess the 5 letter word in the least number of tries')
+        label.move(100,50)
+
+        self.input = QLineEdit(win) #user input guess_1
+        self.input.move(100,100)
+        win.show()
+        sys.exit(app.exec())
+
+
+window = frontend()
+logika = backend()
+backend.Logika_gry()
+
+
